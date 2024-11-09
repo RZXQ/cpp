d27c6f82561294447fa9f3215a9f3b68bd4963dc
Reacher <macryze@icloud.com>
2024-11-09 19:08:08 +0800

    add: 3099-递归求阶乘.cpp

--- recursive/tctm/3099-递归求阶乘/3099-递归求阶乘.cpp
+++ recursive/tctm/3099-递归求阶乘/3099-递归求阶乘.cpp
@@ -13,3 +13,22 @@ int main() {
     cin >> n;
     cout << f(n) << endl;
 }
+
+/*
+标准代码:
+#include<iostream>
+using namespace std;
+//递归计算阶乘
+long long fac(long long n) {
+    if (n == 1)
+        return 1;
+    return fac(n - 1) * n;
+}
+
+int main() {
+    int n = 0;
+    cin >> n;
+    cout << fac(n);
+    return 0;
+}
+*/
