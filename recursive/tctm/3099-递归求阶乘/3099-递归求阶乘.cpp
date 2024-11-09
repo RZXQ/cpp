f35ecb487ac60db4f09cebec9bf64f6b7a34af44
Reacher <macryze@icloud.com>
2024-11-09 19:11:07 +0800

    add: 3099-递归求阶乘.cpp

--- recursive/tctm/3099-递归求阶乘/3099-递归求阶乘.cpp
+++ recursive/tctm/3099-递归求阶乘/3099-递归求阶乘.cpp
@@ -1,3 +1,19 @@
+/*
+Description
+什么是阶乘呢？比如5的阶乘可以写作5！(读作：5的阶乘)。它表示的意思就是1×2×3×4×5，所以5！等于120。现在用递归求n！，将结果输出。
+
+Input
+一个整数n。（1≤n≤10）
+
+Output
+一个整数，n！的结果。
+
+Sample Input 1
+6
+
+Sample Output 1
+720
+*/
 #include <iostream>
 using namespace std;
 
